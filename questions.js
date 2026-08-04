3252637af62e5ba956966cdd5c84c6620bd4efb4
private2547 <abbaismailkaugama@gmail.com>
2026-08-04 01:30:27 +0100

    Add files via upload
    Add Civics Quiz Game files

---
+++ questions.js
@@ -0,0 +1,111 @@
+const questions = [
+    {
+        question: "What is the capital city of Nigeria?",
+        answers: [
+            "Lagos",
+            "Kano",
+            "Abuja",
+            "Kaduna"
+        ],
+        correct: 2
+    },
+
+    {
+        question: "How many states are there in Nigeria?",
+        answers: [
+            "30",
+            "36",
+            "40",
+            "35"
+        ],
+        correct: 1
+    },
+
+    {
+        question: "What are the colours of the Nigerian flag?",
+        answers: [
+            "Green and White",
+            "Green and Red",
+            "Blue and White",
+            "Red and White"
+        ],
+        correct: 0
+    },
+
+    {
+        question: "At what age can a Nigerian citizen vote?",
+        answers: [
+            "16 years",
+            "17 years",
+            "18 years",
+            "21 years"
+        ],
+        correct: 2
+    },
+
+    {
+        question: "What is the supreme law of Nigeria?",
+        answers: [
+            "The Constitution",
+            "The Police Act",
+            "The Education Act",
+            "The Election Act"
+        ],
+        correct: 0
+    },
+
+    {
+        question: "Which arm of government makes laws?",
+        answers: [
+            "Executive",
+            "Judiciary",
+            "Legislature",
+            "Police"
+        ],
+        correct: 2
+    },
+
+    {
+        question: "Which arm of government interprets laws?",
+        answers: [
+            "Executive",
+            "Judiciary",
+            "Legislature",
+            "Military"
+        ],
+        correct: 1
+    },
+
+    {
+        question: "What is democracy?",
+        answers: [
+            "Government by the people",
+            "Government by soldiers",
+            "Government by one person",
+            "Government by kings"
+        ],
+        correct: 0
+    },
+
+    {
+        question: "Which body conducts elections in Nigeria?",
+        answers: [
+            "NIMC",
+            "INEC",
+            "NYSC",
+            "EFCC"
+        ],
+        correct: 1
+    },
+
+    {
+        question: "What should a good citizen do?",
+        answers: [
+            "Break the law",
+            "Avoid paying taxes",
+            "Obey the law and respect others",
+            "Destroy public property"
+        ],
+        correct: 2
+    }
+];
