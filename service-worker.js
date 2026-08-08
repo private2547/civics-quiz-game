6f753d6a222aa0165fe0d5d57fd61ae47ed1a007
private2547 <abbaismailkaugama@gmail.com>
2026-08-08 07:40:13 +0100

    Update service worker to version 2 with new files

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,90 @@
-const CACHE_NAME = "civics-quiz-v1";
+const CACHE_NAME = "civics-quiz-v2";
 
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./questions.js",
-  "./manifest.json",
-  "./logo.png"
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./questions.js",
+    "./manifest.json",
+    "./logo.png",
+    "./correct.mp3",
+    "./wrong.mp3",
+    "./finish.mp3"
 ];
 
+
+// INSTALL
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+
+    self.skipWaiting();
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(FILES_TO_CACHE);
+            })
+    );
 });
 
+
+// ACTIVATE
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(cacheNames => {
+
+            return Promise.all(
+
+                cacheNames.map(cacheName => {
+
+                    if (cacheName !== CACHE_NAME) {
+
+                        return caches.delete(cacheName);
+
+                    }
+
+                })
+
+            );
+
+        })
+
+    );
+
+    self.clients.claim();
+});
+
+
+// FETCH
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+
+    event.respondWith(
+
+        fetch(event.request)
+            .then(response => {
+
+                const responseClone =
+                    response.clone();
+
+                caches.open(CACHE_NAME)
+                    .then(cache => {
+                        cache.put(
+                            event.request,
+                            responseClone
+                        );
+                    });
+
+                return response;
+
+            })
+            .catch(() => {
+
+                return caches.match(event.request);
+
+            })
+
+    );
+
 });
